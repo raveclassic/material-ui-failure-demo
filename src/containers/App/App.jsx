17f600838ec285c02fc82e3af1da6e6370c3023e
raveclassic <raveclassic@live.ru>
2015-11-14 01:13:08 +0400

    initial

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<AppBar label="tset"/>
+			</div>
+		);
+	}
+}
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import {App} from './containers/App/App.jsx';
+
+ReactDom.render(
+	<App/>,
+	document.getElementById('app')
+);
